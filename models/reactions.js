231532a51610fdf4689a40341fedf860d07c4b99
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-08-06 23:53:16 -0700

    added reaction models

---
+++ models/reactions.js
@@ -0,0 +1,32 @@
+const {
+    model,
+    Schema,
+    Types
+} = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max_length: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) = date.toLocaleString(),
+    },
+}, {
+    toJSON: {
+        getters: true,
+    },
+})
+
+module.exports = reactionSchema;
